3157c1e42d86ce8fe32e577b60f514b23048e4db
Alex93Bakun <56162141+Alex93Bakun@users.noreply.github.com>
2021-09-24 13:07:34 +0300

    Mutations: addDirector, addMovie

---
+++ README.md
@@ -0,0 +1,11 @@
+## To start server
+
+Go to server directory
+
+### `cd server`
+### `yarn dev`
+
+### If there is no error, you will see:
+Server listening on port: 5000\
+Connected to DB!\
+### in console.
--- server/app.js
+++ server/app.js
@@ -1,16 +1,29 @@
+require('dotenv').config();
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const mongoose = require('mongoose');
-
 const schema = require('./schema/schema');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const DB_URL = process.env.DB_URL;
 
-app.use('/graphql', graphqlHTTP({
-    schema,
-    graphiql: true,
-}));
+mongoose.connect(DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const dbConnection = mongoose.connection;
+dbConnection.on('error', (error) => console.log('Connection error: ', error));
+dbConnection.once('open', () => console.log('Connected to DB!'));
+
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema,
+        graphiql: true,
+    })
+);
 
 app.listen(PORT, (error) => {
     error
